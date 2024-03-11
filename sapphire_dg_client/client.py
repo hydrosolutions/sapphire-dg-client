3ea8112a6ca9d2c646244ca90d323dd03e259192
tokicnikolaus <nikola.tokic@everstox.com>
2024-03-11 21:21:54 +0100

    Add Era5Land client.

--- sapphire_dg_client/client.py
+++ sapphire_dg_client/client.py
@@ -1,4 +1,5 @@
 from .ecmwf import SapphireECMWFENSClient
+from .era5_land import SapphireEra5LandClient
 from .operational import SapphireOperationalClient
 
 
@@ -9,3 +10,4 @@ class SapphireDGClient:
         self.api_key = api_key
         self.ecmwf_ens = SapphireECMWFENSClient(host, api_key)
         self.operational = SapphireOperationalClient(host, api_key)
+        self.era5_land = SapphireEra5LandClient(host, api_key)
---
+++ sapphire_dg_client/era5_land.py
@@ -0,0 +1,29 @@
+from .client_base import SapphireDGClientBase
+
+
+class SapphireEra5LandClient(SapphireDGClientBase):
+
+    def get_era5_land(
+            self,
+            hru_code: str,
+            date: str,
+            end_date: str,
+            directory: str = "/tmp"
+
+    ):
+        endpoint = f"api/calculations/era5-land/template/RSMinerva?hru_code={hru_code}&start_date={date}"
+        if end_date:
+            endpoint = endpoint + f"&end_date={end_date}"
+
+        resp = self._call_api(
+            method="GET",
+            endpoint=endpoint
+        )
+
+        cd = resp.headers.get('Content-Disposition')
+        if cd and "filename" in cd:
+            filename = cd.split("filename=")[1]
+        else:
+            filename = f"era5_land_{hru_code}_{date}.csv"
+
+        return self._save_file(resp, directory, filename)
